fa0dd8b3571764f5a943b76b6ec135191c84f3d8
Javier de la Hoz <javierdlahoz@Camilo-Correa.local>
2014-06-11 09:20:50 -0500

    Swagger config and gitignore

---
+++ config/autoload/swagger.global.php
@@ -0,0 +1,21 @@
+<?php
+
+return array(
+    'swagger' => array(
+        /**
+         * List a path or paths containing Swagger Annotated classes
+         */
+        'paths' => array(
+            __DIR__ . '/../../module/Application/src/Application/Controller',
+            __DIR__ . '/../../module/User/src/User/Controller',
+        ),
+
+        'resource_options' => array(
+            'output' => 'array',
+            'json_pretty_print' => true, // for outputtype 'json'
+            'defaultBasePath' => null,
+            'defaultApiVersion' => null,
+            'defaultSwaggerVersion' => '1.2',
+        ),
+    )
+);
