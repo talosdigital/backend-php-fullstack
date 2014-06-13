b6d561e0bc52b1cc557394ba821782276da3890b
Javier De la Hoz <javierdlahoz@gmail.com>
2014-06-13 09:16:59 -0500

    improved test

--- module/User/tests/UserTest/IntegrationTests/PhoneTest.php
+++ module/User/tests/UserTest/IntegrationTests/PhoneTest.php
@@ -25,31 +25,29 @@ class PhoneTest extends AbstractTestCase {
 		$this->userService = new UserService($this->getServiceManager());
 	}
 
-	public function testCreatePhone(){
-		$user = $this->userService->findOneBy(array('email' => $this::EMAIL));
-
+	public function createPhone(){
 		$data = array();
 		$data['phonenumber'] = $this::PHONE;
 		$phonenumber = new Phonenumber($data);	
-		$phonenumbers[0] = $phonenumber;
+		return $phonenumber;
+	}
+
+	public function testCreatePhone(){
+		$user = $this->userService->findOneBy(array('email' => $this::EMAIL));
+		$phonenumbers[0] = $this->createPhone();
 
 		$user->setPhonenumbers($phonenumbers);
 		$this->userService->save($user);
 		$phonenumbers = $user->getPhonenumbers();
 
 		$this->assertCount(1, $phonenumbers);
-		$this->assertEquals($data['phonenumber'], $phonenumbers[0]->getPhonenumber());
+		$this->assertEquals($this::PHONE, $phonenumbers[0]->getPhonenumber());
 	}
 
 	public function testCreateTwoOrMorePhones(){
 		$user = $this->userService->findOneBy(array('email' => $this::EMAIL));
-
-		$data = array();
-		$data['phonenumber'] = $this::PHONE;
-		$phonenumber = new Phonenumber($data);
-		
 		for ($i=0; $i < $this::PHONE_QUANTITY; $i++) { 
-			$phonenumbers[$i] = $phonenumber;
+			$phonenumbers[$i] = $this->createPhone();
 		}
 		
 		$user->setPhonenumbers($phonenumbers);
@@ -69,12 +67,8 @@ class PhoneTest extends AbstractTestCase {
 	}
 
 	public function testModifyPhone(){
-		$user = $this->userService->findOneBy(array('email' => $this::EMAIL_WN_EMPTY_ADDRESSES));
-		$data = array();
-		$data['phonenumber'] = $this::PHONE;
-		$phonenumber = new Phonenumber($data);	
-
-		$phonenumbers[0] = $phonenumber;
+		$user = $this->userService->findOneBy(array('email' => $this::EMAIL));
+		$phonenumbers[0] = $this->createPhone();
 		
 		$user->setPhonenumbers($phonenumbers);
 		$this->userService->save($user);
